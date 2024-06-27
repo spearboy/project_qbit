faf790aa7172fba484dedf43f2f32dcb24c432ff
wlsakf23@gmial.com <wlsakf23@gmail.com>
2024-06-27 16:50:18 +0900

    이현 초기 셋팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Header from './components/Header'
-import Main from './components/Main'
-import Home from './pages/Home'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import Yih from "./pages/Yih";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header/>
+      <Header />
       <Main>
         <Routes>
-          <Route path='/' element={<Home/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/yih" element={<Yih />} />
         </Routes>
       </Main>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/pages/Yih.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Yih = () => {
+  return <div>abds</div>;
+};
+
+export default Yih;
