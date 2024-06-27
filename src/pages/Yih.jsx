ad83c3fbc753c432d58107cd88fa3280402b79fb
wlsakf23@gmial.com <wlsakf23@gmail.com>
2024-06-27 17:17:55 +0900

    이현 셋팅 수정

--- src/pages/Yih.jsx
+++ src/pages/Yih.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Yih = () => {
-  return <div>abds</div>;
+  return (
+    <header id="header">
+      <div className="header_wrap">
+        <div className="header_left">
+          <p>직원호출</p>
+        </div>
+        <div className="header_right">
+          <div className=""></div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Yih;
