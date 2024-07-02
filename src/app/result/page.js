fa4243c7a343b3b98fe40f49d1e3e51dcacfbef5
123dd654 <161431124+123dd654@users.noreply.github.com>
2024-07-02 17:28:00 +0900

    scss update

--- src/app/result/page.js
+++ src/app/result/page.js
@@ -1,11 +1,17 @@
+import Line from "@/components/common/Line";
 import My_basket from "@/components/result/My_basket";
 import Your_basket from "@/components/result/Your_basket";
 
 export default function Result() {
     return (
-        <div className="container">
-            <My_basket />
-            <Your_basket />
-        </div>
+        <>
+            <div className="container">
+                <My_basket />
+            </div>
+                <Line />
+            <div className="container">
+                <Your_basket />
+            </div>
+        </>
     );
 }
--- src/components/result/My_basket.jsx
+++ src/components/result/My_basket.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 
-const Shopping_basket = () => {
+const My_basket = () => {
+  
   return (
       <div className='mymenu'>
         <h2>내가 담은 메뉴</h2>
-        <p>담은 메뉴가 없어요</p>
-      </div>
+        <p style={{ display: 'none' }}>담은 메뉴가 없어요</p>
+          <div className='detail'>
+            <ul>
+              <li>병현 멍청 덮밥</li>
+              <li>양념추가(+500원)</li>
+              <li>18,500원</li>
+            </ul>
+          <i className='icon-24-delete'></i>
+          </div>
+        </div>
+
   )
 }
 
-export default Shopping_basket
+export default My_basket
