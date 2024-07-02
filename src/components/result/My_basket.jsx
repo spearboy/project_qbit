12a8c4d1926e8639917919361fcfda033af55263
123dd654 <161431124+123dd654@users.noreply.github.com>
2024-07-02 19:24:52 +0900

    reault_menu update

--- src/components/result/My_basket.jsx
+++ src/components/result/My_basket.jsx
@@ -4,9 +4,10 @@ const My_basket = () => {
   
   return (
       <div className='mymenu'>
-        <h2>내가 담은 메뉴</h2>
-        <p style={{ display: 'none' }}>담은 메뉴가 없어요</p>
-          <div className='detail'>
+        <div className='detail_menu' style={{ display: 'none' }}>
+          <h2>내가 담은 메뉴</h2>
+          <p>담은 메뉴가 없어요</p>
+          <div className='detail' style={{ display: 'none' }}>
             <ul>
               <li>병현 멍청 덮밥</li>
               <li>양념추가(+500원)</li>
@@ -15,6 +16,14 @@ const My_basket = () => {
           <i className='icon-24-delete'></i>
           </div>
         </div>
+        <div className='coin'>
+          <ul>
+            <li>총 <span>4</span>개</li>
+            <li>43,500원</li>
+            <li>결제는 카운터에서 해주세요</li>
+          </ul>
+        </div>
+      </div>
 
   )
 }
--- src/components/result/Your_basket.jsx
+++ src/components/result/Your_basket.jsx
@@ -3,8 +3,31 @@ import React from 'react'
 const Your_basket = () => {
   return (
       <div className='youmenu'>
-        <h2>멤버가 담은 메뉴</h2>
-        <p>담은 메뉴가 없어요</p>
+        <div className='detail_menu'  style={{ display: 'none' }}>
+          <h2>멤버가 담은 메뉴</h2>
+          <p>담은 메뉴가 없어요</p> 
+        </div> 
+        <div className='detail_menuplus'>
+            <ul>
+              <li>병현 멍청 덮밥</li>
+              <li>기본(+0원), 양념추가(+500원)</li>
+              <li>18,500원</li>
+            </ul>
+            <ul>
+              <li>큐빗 돈코츠 라멘</li>
+              <li>기본(+0원)</li>
+              <li>10,000원</li>
+            </ul>
+            <ul>
+              <li>큐빗 마제소바</li>
+              <li>기본(+0원)</li>
+              <li>9,000원</li>
+            </ul>
+            <ul>
+              <li>지나가 좋아하는 교자</li>
+              <li>6,000원</li>
+            </ul>
+        </div>
       </div>
   )
 }
