c61891d58721297044306886b545853dcf4de291
spearboy <tsagaanaa123@gmail.com>
2024-07-02 14:35:19 +0900

    update

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,5 +1,11 @@
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className="top__header">
+      <div className="call__button">직원호출</div>
+      <div className="right__action__buttons">
+        <i className="icon-24-receipt"></i>
+        <i className="icon-24-bag"></i>
+      </div>
+    </div>
   )
 }
