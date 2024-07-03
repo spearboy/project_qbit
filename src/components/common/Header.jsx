e99b477022b59418163de726f9a0327108e9430a
spearboy <tsagaanaa123@gmail.com>
2024-07-03 14:17:04 +0900

    update

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,16 @@
+import Link from "next/link"
+
 export const Header = () => {
   return (
     <div className="top__header">
       <div className="call__button">직원호출</div>
       <div className="right__action__buttons">
-        <i className="icon-24-receipt"></i>
-        <i className="icon-24-bag"></i>
+        <Link href={'/result'}>
+          <i className="icon-24-receipt"></i>
+        </Link>
+        <Link href={'/result'}>
+          <i className="icon-24-bag"></i>
+        </Link>
       </div>
     </div>
   )
