11b46249856dc1e59b692c7accfb595db3d49681
spearboy <tsagaanaa123@gmail.com>
2024-07-08 14:16:05 +0900

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,27 @@
+"use client";
+import { useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
+import { gsap } from "gsap";
+import { logoImg } from "@/assets/images/logo.svg"
+import Image from "next/image";
 
 export default function Home() {
+  const router = useRouter();
+  const logoRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(logoRef.current, { rotation: 360, duration: 3, ease: "power1.inOut" });
+
+    const timer = setTimeout(() => {
+      // router.push("/main");
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <div className="container">home</div>
+    <div className="randing_page_wrapper">
+      <Image ref={logoRef} src={logoImg} alt="Logo" className="logo" />
+    </div>
   );
 }
