ed961f2cf2cbb319cf8875a645f0a59c19f330fd
spearboy <tsagaanaa123@gmail.com>
2024-07-09 17:27:09 +0900

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,25 @@
 "use client";
+
 import { useRouter } from "next/navigation";
+import logoSvg from "../../public/img/logo.svg"
+import Image from "next/image";
 
 export default function Home() {
   const router = useRouter();
   setTimeout(() => {
-    router.push("/main");
+    // router.push("/main");
   }, 3000);
 
   return (
     <div className="randing_page_wrapper">
+      <Image src={logoSvg} width={30} alt="qbit logo" height={30}/>
+      <div className="firtst_loading">
+        <span className="loading_span"></span>
+        <span className="loading_span"></span>
+        <span className="loading_span"></span>
+        <span className="loading_span"></span>
+        <span className="loading_span"></span>
+      </div>
     </div>
   );
 }
